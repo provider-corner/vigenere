d9f1e3bd056dac528950e0bf703704e369c85e3c
Richard Levitte <richard@levitte.org>
2019-10-28 11:46:49 +0100

    Make it possible to set the key length with current OpenSSL functionality
    This makes it an error not having set the key length.
    
    Indentation gets cleaned up as well where the code is touched.

--- vigenere.c
+++ vigenere.c
@@ -26,6 +26,12 @@ static OSSL_OP_cipher_update_fn vigenere_update;
 static OSSL_OP_cipher_final_fn vigenere_final;
 static OSSL_OP_cipher_dupctx_fn vigenere_dupctx;
 static OSSL_OP_cipher_freectx_fn vigenere_freectx;
+static OSSL_OP_cipher_get_params_fn vigenere_get_params;
+static OSSL_OP_cipher_gettable_params_fn vigenere_gettable_params;
+static OSSL_OP_cipher_set_ctx_params_fn vigenere_set_ctx_params;
+static OSSL_OP_cipher_get_ctx_params_fn vigenere_get_ctx_params;
+static OSSL_OP_cipher_settable_ctx_params_fn vigenere_settable_ctx_params;
+static OSSL_OP_cipher_gettable_ctx_params_fn vigenere_gettable_ctx_params;
 
 static void *vigenere_newctx(void *vprovctx)
 {
@@ -38,12 +44,14 @@ static void *vigenere_newctx(void *vprovctx)
 
 static void vigenere_cleanctx(void *vctx)
 {
-  struct vigenere_ctx_st *ctx = vctx;
+    struct vigenere_ctx_st *ctx = vctx;
 
-  if (ctx == NULL)
-    return;
-  free(ctx->key);
-  memset(ctx, '\0', sizeof(*ctx));
+    if (ctx == NULL)
+        return;
+    free(ctx->key);
+    ctx->key = NULL;
+    ctx->keypos = 0;
+    ctx->enc = 0;
 }
 
 static void *vigenere_dupctx(void *vctx)
@@ -53,15 +61,18 @@ static void *vigenere_dupctx(void *vctx)
 
     dst = vigenere_newctx(NULL);
     if (dst == NULL)
-      return NULL;
-    if (src->keyl > 0) {
-      if ((dst->key = malloc(src->keyl)) == NULL) {
-        vigenere_freectx(dst);
         return NULL;
-      }
-      memcpy(dst->key, src->key, src->keyl);
-      dst->keyl = src->keyl;
+
+    dst->keyl = src->keyl;
+
+    if (src->key != NULL) {
+        if ((dst->key = malloc(src->keyl)) == NULL) {
+            vigenere_freectx(dst);
+            return NULL;
+        }
+        memcpy(dst->key, src->key, src->keyl);
     }
+
     dst->keypos = src->keypos;
     dst->enc = src->enc;
 
@@ -84,10 +95,9 @@ static int vigenere_encrypt_init(void *vctx,
 {
     struct vigenere_ctx_st *ctx = vctx;
 
-    vigenere_cleanctx(ctx);
     if (keyl == (size_t)-1)
-      /* Because that's what test_vigenere.c gives */
-      keyl = 16;
+        return 0;
+    vigenere_cleanctx(ctx);
     ctx->key = malloc(keyl);
     memcpy(ctx->key, key, keyl);
     ctx->keyl = keyl;
@@ -104,10 +114,9 @@ static int vigenere_decrypt_init(void *vctx,
     struct vigenere_ctx_st *ctx = vctx;
     size_t i;
 
-    vigenere_cleanctx(ctx);
     if (keyl == (size_t)-1)
-      /* Because that's what test_vigenere.c gives */
-      keyl = 16;
+        return 0;
+    vigenere_cleanctx(ctx);
     ctx->key = malloc(keyl);
     for (i = 0; i < keyl; i++)
         ctx->key[i] = 256 - key[i];
@@ -145,14 +154,69 @@ static int vigenere_final(void *vctx,
     return 1;
 }
 
+/* Parameters that libcrypto can get from this implementation */
+static const OSSL_PARAM *vigenere_gettable_params(void)
+{
+    static const OSSL_PARAM table[] = {
+        OSSL_PARAM_size_t("blocksize", NULL),
+        OSSL_PARAM_END
+    };
+
+    return table;
+}
+
 static int vigenere_get_params(OSSL_PARAM params[])
 {
-  OSSL_PARAM *p;
+    OSSL_PARAM *p;
 
-  if ((p = OSSL_PARAM_locate(params, "blocksize")) != NULL)
-    if (!OSSL_PARAM_set_size_t(p, 1))
-      return 0;
-  return 1;
+    if ((p = OSSL_PARAM_locate(params, "blocksize")) != NULL)
+        if (!OSSL_PARAM_set_size_t(p, 1))
+            return 0;
+    return 1;
+}
+
+static const OSSL_PARAM *vigenere_gettable_ctx_params(void)
+{
+    static const OSSL_PARAM table[] = {
+        OSSL_PARAM_size_t("keylen", NULL),
+        OSSL_PARAM_END
+    };
+
+    return table;
+}
+
+static int vigenere_get_ctx_params(void *vctx, OSSL_PARAM params[])
+{
+    struct vigenere_ctx_st *ctx = vctx;
+    OSSL_PARAM *p;
+
+    if (ctx->keyl > 0
+        && (p = OSSL_PARAM_locate(params, "keylen")) != NULL
+        && !OSSL_PARAM_set_size_t(p, ctx->keyl))
+        return 0;
+    return 1;
+}
+
+/* Parameters that libcrypto can send to this implementation */
+static const OSSL_PARAM *vigenere_settable_ctx_params(void)
+{
+    static const OSSL_PARAM table[] = {
+        OSSL_PARAM_size_t("keylen", NULL),
+        OSSL_PARAM_END
+    };
+
+    return table;
+}
+
+static int vigenere_set_ctx_params(void *vctx, const OSSL_PARAM params[])
+{
+    struct vigenere_ctx_st *ctx = vctx;
+    const OSSL_PARAM *p;
+
+    if ((p = OSSL_PARAM_locate_const(params, "keylen")) != NULL
+        && !OSSL_PARAM_get_size_t(p, &ctx->keyl))
+        return 0;
+    return 1;
 }
 
 static const OSSL_DISPATCH vigenere_functions[] = {
@@ -164,12 +228,20 @@ static const OSSL_DISPATCH vigenere_functions[] = {
     { OSSL_FUNC_CIPHER_DUPCTX, (funcptr_t)vigenere_dupctx },
     { OSSL_FUNC_CIPHER_FREECTX, (funcptr_t)vigenere_freectx },
     { OSSL_FUNC_CIPHER_GET_PARAMS, (funcptr_t)vigenere_get_params },
+    { OSSL_FUNC_CIPHER_GETTABLE_PARAMS, (funcptr_t)vigenere_gettable_params },
+    { OSSL_FUNC_CIPHER_GET_CTX_PARAMS, (funcptr_t)vigenere_get_ctx_params },
+    { OSSL_FUNC_CIPHER_GETTABLE_CTX_PARAMS,
+      (funcptr_t)vigenere_gettable_ctx_params },
+    { OSSL_FUNC_CIPHER_SET_CTX_PARAMS, (funcptr_t)vigenere_set_ctx_params },
+    { OSSL_FUNC_CIPHER_SETTABLE_CTX_PARAMS,
+      (funcptr_t)vigenere_settable_ctx_params },
     { 0, NULL }
 };
 
-static const OSSL_ALGORITHM vigenere_ciphers[] =
-  { { "vigenere", NULL, vigenere_functions },
-    { NULL , NULL, NULL } };
+static const OSSL_ALGORITHM vigenere_ciphers[] = {
+    { "vigenere", NULL, vigenere_functions },
+    { NULL , NULL, NULL }
+};
 
 static const OSSL_ALGORITHM *vigenere_operation(void *vprovctx,
                                                 int operation_id,
