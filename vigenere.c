a41792ec6a0a82ccdf639e8e9a52e5b627f9a9a3
Richard Levitte <richard@levitte.org>
2023-01-05 21:20:37 +0100

    Make the x.author value a quoted string
    This way, OpenSSL's property value parser won't fail.

--- vigenere.c
+++ vigenere.c
@@ -407,7 +407,7 @@ static const OSSL_DISPATCH vigenere_functions[] = {
 
 /* The table of ciphers this provider offers */
 static const OSSL_ALGORITHM vigenere_ciphers[] = {
-    { "vigenere:1.3.6.1.4.1.5168.4711.22087.1", "x.author=" AUTHOR,
+    { "vigenere:1.3.6.1.4.1.5168.4711.22087.1", "x.author='" AUTHOR "'",
       vigenere_functions },
     { NULL, NULL, NULL }
 };
